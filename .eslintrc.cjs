0a739ea923a2041abbd254ad7a3c1d99f0e1a145
ErezKon <erezkon@gmail.com>
2026-08-08 00:55:43 +0300

    [pacman5]-[SC-001]-chore: final cleanup for pacman5/chore/scaffold

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Add any project-specific rules here
+    'prettier/prettier': 'error',
+  },
+};
